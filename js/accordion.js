ed66cae57f49b944a8c78a6755eef0773d18c5d6
Valeria-7 <68329127+Valeria-7@users.noreply.github.com>
2021-05-19 16:27:58 +0300

    Add files via upload
    add smoothScroll and tabs

--- js/accordion.js
+++ js/accordion.js
@@ -3,36 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const featureLinkElems = document.querySelectorAll(".feature__link");
     const featureSubElems = document.querySelectorAll(".feature-sub");
 
-
-    /*for(let i = 0; i < featureLinkElems.length; i++) {
-        featureLinkElems[i].addEventListener('click', () => {
-            featureLinkElems[i].classList.toggle("feature__link_active");
-            featureSubElems[i].classList.toggle("hidden"); 
-        }); 
-    }*/
-    
-    featureLinkElems.forEach((btn, index) => {
-        
+    featureLinkElems.forEach((btn, index) => {   
         btn.addEventListener('click', () => {
-            
-            
-            /*console.log(btn.classList.contains("feature__link_active"));
+
             if (btn.classList.contains("feature__link_active")) { 
                 featureSubElems[index].classList.add("hidden");
                 btn.classList.remove("feature__link_active");
-                break;
-            }; */
-            
-            featureSubElems.forEach((featureSubElem) => {
-                featureSubElem.classList.add("hidden");
-            });
-            featureLinkElems.forEach((featureLinkElem) => {
-                featureLinkElem.classList.remove("feature__link_active");
-            });
-
-            featureSubElems[index].classList.remove("hidden");
-            btn.classList.add("feature__link_active");
-
+                
+            } else {
+                featureSubElems.forEach((featureSubElem) => {
+                    featureSubElem.classList.add("hidden");
+                });
+                featureLinkElems.forEach((featureLinkElem) => {
+                    featureLinkElem.classList.remove("feature__link_active");
+                });
+                featureSubElems[index].classList.remove("hidden");
+                btn.classList.add("feature__link_active");
+            }
         });
     });
     
